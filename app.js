115b80a5b5addb05ec49a008cccd5005da569d3b
unknown <joseph-tds@mail.ru>
2020-02-08 20:03:03 +0800

    change production connection variable in app.js

--- app.js
+++ app.js
@@ -48,10 +48,10 @@ app.use(function(err, req, res, next) {
 });
 
 //var dev_db_url = 'mongodb+srv://Joseph:Gq7qTpbq6TZEXgYz@cluster0-iuis4.mongodb.net/local_library';
-var prod_db_url = 'mongodb+srv://User_for_production:a5Bor4sik3cvO@cluster0-iuis4.mongodb.net/ProductionDB';
-var MONGODB = process.env.MONGODB_URI || prod_db_url;
-console.log(MONGODB);
-mongoose.connect(MONGODB,{useNewUrlParser:true, useFindAndModify:false, useUnifiedTopology: true } );
+//var prod_db_url = 'mongodb+srv://User_for_production:a5Bor4sik3cvO@cluster0-iuis4.mongodb.net/ProductionDB';
+var dev_db_url = 'mongodb+srv://User_for_production:a5Bor4sik3cvO@cluster0-mbdj7.mongodb.net/ProductionDB?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB,{useNewUrlParser:true, useFindAndModify:false, useUnifiedTopology: true } );
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
